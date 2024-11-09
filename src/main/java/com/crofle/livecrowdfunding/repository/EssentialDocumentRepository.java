67b2315377e6de2119b03452443f84c1371624d3
yejinCodes <yehi97@gmail.com>
2024-11-09 17:37:16 +0900

    Feat: testing enitities Create, Read

---
+++ src/main/java/com/crofle/livecrowdfunding/repository/EssentialDocumentRepository.java
@@ -0,0 +1,7 @@
+package com.crofle.livecrowdfunding.repository;
+
+import com.crofle.livecrowdfunding.domain.entity.EssentialDocument;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EssentialDocumentRepository extends JpaRepository<EssentialDocument, Long> {
+}
--- src/test/java/com/crofle/livecrowdfunding/TestEntity.java
+++ src/test/java/com/crofle/livecrowdfunding/TestEntity.java
@@ -1,17 +1,19 @@
 package com.crofle.livecrowdfunding;
 
+import com.crofle.livecrowdfunding.domain.entity.EssentialDocument;
+import com.crofle.livecrowdfunding.domain.entity.RatePlan;
 import com.crofle.livecrowdfunding.domain.entity.User;
 import com.crofle.livecrowdfunding.domain.enums.UserStatus;
+import com.crofle.livecrowdfunding.repository.RatePlanRepository;
 import com.crofle.livecrowdfunding.repository.UserRepository;
-import jakarta.persistence.EntityManager;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
 
 @SpringBootTest
 @Rollback(false)
@@ -19,6 +21,30 @@ public class TestEntity {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private RatePlanRepository ratePlanRepository;
+
+    @Test
+    public void ratePlanTest(){
+        RatePlan ratePlanAdd = RatePlan.builder()
+                .planName("Ultimate")
+                .charge(Boolean.TRUE)
+                .build();
+        RatePlan savePlan = ratePlanRepository.save(ratePlanAdd);
+        RatePlan findPlan = ratePlanRepository.findById(savePlan.getId())
+                .orElseThrow( ()-> new RuntimeException("nope"));
+        System.out.println(savePlan.getId());
+        System.out.println(findPlan.getPlanName() + " price: " + findPlan.getCharge());
+    }
+
+    @Test
+    public void essentialDocTest(){
+        EssentialDocument essentialDocument = EssentialDocument.builder()
+
+                .build();
+
+    }
 
     @Test
     public void userEntityTest() {
