6a2307a79fe03636e6fd655dca7530a5ecffe27a
EdenKwon <ybinn@caresquare.kr>
2024-11-10 18:14:38 +0900

    FEAT : project 간단 service 구현 및 테스트 example

--- src/main/java/com/crofle/livecrowdfunding/service/ProjectServiceImpl.java
+++ src/main/java/com/crofle/livecrowdfunding/service/ProjectServiceImpl.java
@@ -1,26 +1,30 @@
 package com.crofle.livecrowdfunding.service;
 
 import com.crofle.livecrowdfunding.domain.entity.Project;
+import com.crofle.livecrowdfunding.dto.ProjectDetailDTO;
 import com.crofle.livecrowdfunding.repository.ProjectRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
 @Service
-public class ProjectServiceImpl {
+@Log4j2
+@RequiredArgsConstructor
+public class ProjectServiceImpl implements ProjectService {
 
     private final ProjectRepository projectRepository;
+    private final ModelMapper modelMapper;
 
-    @Autowired
-    public ProjectServiceImpl(ProjectRepository projectRepository) {
-        this.projectRepository = projectRepository;
-    }
-
-    public void findProjectDetail(Long id) {
+    public ProjectDetailDTO findProjectDetail(Long id) {
         Optional<Project> project = Optional.of(projectRepository.findById(id).get());
 
+        ProjectDetailDTO projectDetailDTO = modelMapper.map(project.get(), ProjectDetailDTO.class);
 
+        return projectDetailDTO;
     }
 
 }
