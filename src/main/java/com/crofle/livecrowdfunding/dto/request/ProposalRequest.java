f8bdedf6bad250f84c46c7e1c79781e9546029ab
EdenKwon <ybinn@caresquare.kr>
2024-11-18 20:59:04 +0900

    FIX : FundingDocument 필드 명칭 수정

--- src/main/java/com/crofle/livecrowdfunding/dto/request/ProposalRequest.java
+++ src/main/java/com/crofle/livecrowdfunding/dto/request/ProposalRequest.java
@@ -9,5 +9,5 @@ import lombok.NoArgsConstructor;
 @Data
 public class ProposalRequest {
     private String projectDocument;
-    private String FundingDocument;
+    private String fundingDocument;
 }
